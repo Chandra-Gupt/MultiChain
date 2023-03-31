0f89a058faf5b2b0284ff94c41b15a42b0110987
Chandra Gupt <cgmaurya477@gmail.com>
2023-03-31 17:36:52 +0530

    making some changes

--- components/Walletconnect.js
+++ components/Walletconnect.js
@@ -196,7 +196,7 @@ function Walletconnect() {
   console.log( "connected................>");
   async function targetTokenValue() {
    const value1= (tokenvalue * (destToken?.SwapFeeRatePerMillion)) / 100;
-   if(tokenvalue>Number(destToken?.MinimumSwap)){
+   if(tokenvalue>=Number(destToken?.MinimumSwap)){
     if(value1<=Number(destToken?.MinimumSwapFee)){
       setTargetTokenValue(tokenvalue-Number(destToken?.MinimumSwapFee));
      }else {
