55eab4b83305dc8768c182226347c6c65f651e79
Chandra Gupt <cgmaurya477@gmail.com>
2023-05-04 12:59:20 +0530

    making some changes

--- components/Walletconnect.js
+++ components/Walletconnect.js
@@ -279,49 +279,52 @@ function Walletconnect() {
     setTokenValue(token_value);
   }
 
-  // async function tokenBalance(){
-  //   var abi1 = [
+  async function tokenBalance(){
+    var abi1 = [
      
       
-  //     {
-  //       "constant":true,
-  //       "inputs":[{"name":"_owner","type":"address"}],
-  //       "name":"balanceOf",
-  //       "outputs":[{"name":"balance","type":"uint256"}],
-  //       "type":"function"
-  //     },
-  //     // decimals
-  //     {
-  //       "constant":true,
-  //       "inputs":[],
-  //       "name":"decimals",
-  //       "outputs":[{"name":"","type":"uint8"}],
-  //       "type":"function"
-  //     }
-  //   ];
-  //   const web3 = new Web3(Web3.givenProvider);
-  //   const anyToken = selectData?.address;
-  //   const walletAddress=address;
-  //   if(anyToken?.length>=10){
-  //     const tokenContract=new web3.eth.Contract(abi1,anyToken)
-  //   }
+      {
+        "constant":true,
+        "inputs":[{"name":"_owner","type":"address"}],
+        "name":"balanceOf",
+        "outputs":[{"name":"balance","type":"uint256"}],
+        "type":"function"
+      },
+      // decimals
+      {
+        "constant":true,
+        "inputs":[],
+        "name":"decimals",
+        "outputs":[{"name":"","type":"uint8"}],
+        "type":"function"
+      }
+    ];
+    const web3 = new Web3(Web3.givenProvider);
+    const anyToken = selectData?.address;
+    const walletAddress=address;
     
-  //   if(address&&anyToken){
-  //     try{
-  //       let result = await tokenContract.methods.balanceOf(walletAddress).call();
-  //      const balance= web3.utils.fromWei(result, "ether")
-  //       setWalletBalance(balance)
-  //     }catch(err){
-  //       console.log(err)
-  //     }
+      const tokenContract=new web3.eth.Contract(abi1,anyToken)
+    
+    
+    if(address&&anyToken){
+      try{
+        let result = await tokenContract.methods.balanceOf(walletAddress).call();
+        const decimals = await tokenContract.methods.decimals().call();
+        
+       const balance= result/Math.pow(10, decimals)
+       console.log(balance,"balance...................")
+        setWalletBalance(balance)
+      }catch(err){
+        console.log(err)
+      }
      
-  //   }
+    }
     
-  // }
+  }
 
-  // useEffect(() => {
-  //   tokenBalance();
-  // }, [selectData,chainId]);
+  useEffect(() => {
+    tokenBalance();
+  }, [selectData,chainId]);
 
   async function toAddress(e){
    const Address=e.target.value
